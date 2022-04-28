4de3aafe1bf0fcf1c50d587f2c6cabac4062c9ba
mosmo46 <73312834+mosmo46@users.noreply.github.com>
2022-04-28 18:16:14 +0300

    commit

---
+++ Project.OpenSky.Model/OpenSkyModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.OpenSky.Model
+{
+    public class OpenSkyModel
+    {
+
+            public int time { get; set; }
+            public object[][] states { get; set; }
+
+    }
+}
---
+++ Project.OpenSky.UI/Form1.cs
@@ -0,0 +1,53 @@
+﻿using Project.OpenSky.Dal;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project.OpenSky.UI
+{
+    public partial class Form1 : Form
+    {
+      public List<string> AuthorList = new List<string>();
+
+       // public string[] names;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            OpenSkyDal c = new OpenSkyDal();
+           await c.GetAllData();
+       
+            ListCantry();
+        }
+
+        public async void ListCantry()
+        {
+            OpenSkyDal c = new OpenSkyDal();
+           var list =  await c.GetAllData();
+            var bigList = list.states;
+
+            listView1.View = View.Details;
+
+            listView1.Columns.Add("Country Name");
+
+            for (int i = 0; i < bigList.Length; i++)
+            {
+            listView1.Items.Add(new ListViewItem(new string[] { bigList[i][2].ToString() }));
+
+            }
+
+           
+
+        }
+    }
+}
